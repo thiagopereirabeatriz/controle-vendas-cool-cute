a8536ecb86137f1b7edb696c32c92aa1422b5198
Bruno Alves <bruno.ar.avl@gmail.com>
2016-03-26 11:43:27 -0300

    #Melhorias nos arquivos do Banco de Dados #Atualização do cronograma e inclusão de uma planilha para controle das datas de término.

--- DOCUMENTAÇÃO/Banco de Dados/SCRIPTS/CRIACAO.sql
+++ DOCUMENTAÇÃO/Banco de Dados/SCRIPTS/CRIACAO.sql
@@ -1,173 +1,121 @@
 CREATE TABLE StatusPedido (
-  stpCodigo INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+  stpCodigo INTEGER AUTO_INCREMENT,
   stpDescricao VARCHAR(255) NOT NULL,
   PRIMARY KEY(stpCodigo)
 );
-
 CREATE TABLE StatusAnuncio (
-  staCodigo INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+  staCodigo INTEGER AUTO_INCREMENT,
   staDescricao VARCHAR(255) NOT NULL,
   PRIMARY KEY(staCodigo)
 );
-
 CREATE TABLE Produto (
-  proCodigo INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+  proCodigo INTEGER AUTO_INCREMENT,
   proNome VARCHAR(255) NOT NULL,
-  proEstoqueMin INTEGER UNSIGNED NULL DEFAULT 0,
+  proEstoqueMin INTEGER UNSIGNED DEFAULT 0,
   proIsAtivo BOOL NOT NULL,
   PRIMARY KEY(proCodigo)
 );
-
 CREATE TABLE Usuario (
-  usrCodigo INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+  usrCodigo INTEGER AUTO_INCREMENT,
   usrLogin VARCHAR(255) NOT NULL,
   usrSenha VARCHAR(255) NOT NULL,
   usrIsAdministrador BOOL NOT NULL,
   usrIsAtivo BOOL NOT NULL,
   PRIMARY KEY(usrCodigo)
 );
-
 CREATE TABLE TipoAvaliacoes (
-  tivCodigo INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+  tivCodigo INTEGER AUTO_INCREMENT,
   tivDescricao INTEGER UNSIGNED NOT NULL,
   PRIMARY KEY(tivCodigo)
 );
-
 CREATE TABLE TipoAnuncio (
-  tiaCodigo INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+  tiaCodigo INTEGER AUTO_INCREMENT,
   tiaDescricao VARCHAR(255) NOT NULL,
   tiaPercentual DECIMAL NOT NULL,
   PRIMARY KEY(tiaCodigo)
 );
-
 CREATE TABLE Cliente (
-  cliCodigo INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+  cliCodigo INTEGER AUTO_INCREMENT,
   cliNome VARCHAR(255) NOT NULL,
   cliEmail VARCHAR(255) NOT NULL,
   PRIMARY KEY(cliCodigo)
 );
-
 CREATE TABLE OutrasMovimentacoes (
-  movCodigo INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+  movCodigo INTEGER AUTO_INCREMENT,
   movDescricao VARCHAR(255) NOT NULL,
   movValor DECIMAL NOT NULL,
   movData DATE NOT NULL,
   PRIMARY KEY(movCodigo)
 );
-
 CREATE TABLE Entrada (
-  entCodigo INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+  entCodigo INTEGER AUTO_INCREMENT,
   entLote VARCHAR(255) NOT NULL,
   entValorTotal DECIMAL NOT NULL,
-  entObservacao VARCHAR(255) NULL,
+  entObservacao VARCHAR(255),
   PRIMARY KEY(entCodigo)
 );
-
 CREATE TABLE TipoMovimentacao (
-  timCodigo INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
-  movCodigo INTEGER UNSIGNED NOT NULL,
+  timCodigo INTEGER AUTO_INCREMENT,
+  movCodigo INTEGER NOT NULL,
   timDescricao VARCHAR(255) NOT NULL,
   timOperacao BOOL NOT NULL,
   PRIMARY KEY(timCodigo),
-  FOREIGN KEY(movCodigo)
-    REFERENCES OutrasMovimentacoes(movCodigo)
-      ON DELETE NO ACTION
-      ON UPDATE NO ACTION
+  FOREIGN KEY(movCodigo) REFERENCES OutrasMovimentacoes(movCodigo)
 );
-
 CREATE TABLE ProdutoEstoque (
-  estCodigo INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
-  proCodigo INTEGER UNSIGNED NOT NULL,
+  estCodigo INTEGER AUTO_INCREMENT,
+  proCodigo INTEGER NOT NULL,
   estPreco DECIMAL NOT NULL,
   estQuantidade INTEGER NOT NULL,
   PRIMARY KEY(estCodigo),
-  FOREIGN KEY(proCodigo)
-    REFERENCES Produto(proCodigo)
-      ON DELETE NO ACTION
-      ON UPDATE NO ACTION
+  FOREIGN KEY(proCodigo) REFERENCES Produto(proCodigo)
 );
-
 CREATE TABLE Anuncio (
-  anuCodigo INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
-  staCodigo INTEGER UNSIGNED NOT NULL,
-  tiaCodigo INTEGER UNSIGNED NOT NULL,
+  anuCodigo INTEGER AUTO_INCREMENT,
+  staCodigo INTEGER NOT NULL,
+  tiaCodigo INTEGER NOT NULL,
   anuDescricao VARCHAR(255) NOT NULL,
   anuPreco DECIMAL NOT NULL,
   anuDataCriacao DATE NOT NULL,
   anuIsAtivo BOOL NOT NULL,
   PRIMARY KEY(anuCodigo),
-  FOREIGN KEY(tiaCodigo)
-    REFERENCES TipoAnuncio(tiaCodigo)
-      ON DELETE NO ACTION
-      ON UPDATE NO ACTION,
-  FOREIGN KEY(staCodigo)
-    REFERENCES StatusAnuncio(staCodigo)
-      ON DELETE NO ACTION
-      ON UPDATE NO ACTION
+  FOREIGN KEY(tiaCodigo) REFERENCES TipoAnuncio(tiaCodigo),
+  FOREIGN KEY(staCodigo) REFERENCES StatusAnuncio(staCodigo)
 );
-
 CREATE TABLE ItensEntrada (
-  proCodigo INTEGER UNSIGNED NOT NULL,
-  entCodigo INTEGER UNSIGNED NOT NULL,
-  iteQuantidade INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
-  iteValor DECIMAL NULL,
-  iteValorTotal DECIMAL NULL
-  FOREIGN KEY(entCodigo)
-    REFERENCES Entrada(entCodigo)
-      ON DELETE NO ACTION
-      ON UPDATE NO ACTION,
-  FOREIGN KEY(proCodigo)
-    REFERENCES Produto(proCodigo)
-      ON DELETE NO ACTION
-      ON UPDATE NO ACTION
+  proCodigo INTEGER NOT NULL,
+  entCodigo INTEGER NOT NULL,
+  iteQuantidade INTEGER UNSIGNED NOT NULL,
+  iteValor DECIMAL,
+  iteValorTotal DECIMAL,
+  FOREIGN KEY(entCodigo) REFERENCES Entrada(entCodigo),
+  FOREIGN KEY(proCodigo) REFERENCES Produto(proCodigo)
 );
-
 CREATE TABLE Pedido (
-  pedCodigo INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
-  tivCodigo INTEGER UNSIGNED NOT NULL,
-  stpCodigo INTEGER UNSIGNED NOT NULL,
-  anuCodigo INTEGER UNSIGNED NOT NULL,
-  cliCodigo INTEGER UNSIGNED NOT NULL,
+  pedCodigo INTEGER AUTO_INCREMENT,
+  tivCodigo INTEGER NOT NULL,
+  stpCodigo INTEGER NOT NULL,
+  anuCodigo INTEGER NOT NULL,
+  cliCodigo INTEGER NOT NULL,
   pedDataVenda DATE NOT NULL,
   pedValorFrete DECIMAL NOT NULL DEFAULT 0,
-  pedDataPostagem DATE NULL,
-  pedDataEntrega DATE NULL,
+  pedDataPostagem DATE,
+  pedDataEntrega DATE,
   pedValorTotal DECIMAL NOT NULL,
-  pedCodigoPostagem VARCHAR(255) NULL,
-  pedObservacao VARCHAR(255) NULL,
+  pedCodigoPostagem VARCHAR(255),
+  pedObservacao VARCHAR(255),
   PRIMARY KEY(pedCodigo),
-  FOREIGN KEY(cliCodigo)
-    REFERENCES Cliente(cliCodigo)
-      ON DELETE NO ACTION
-      ON UPDATE NO ACTION,
-  FOREIGN KEY(anuCodigo)
-    REFERENCES Anuncio(anuCodigo)
-      ON DELETE NO ACTION
-      ON UPDATE NO ACTION,
-  FOREIGN KEY(stpCodigo)
-    REFERENCES StatusPedido(stpCodigo)
-      ON DELETE NO ACTION
-      ON UPDATE NO ACTION,
-  FOREIGN KEY(tivCodigo)
-    REFERENCES TipoAvaliacoes(tivCodigo)
-      ON DELETE NO ACTION
-      ON UPDATE NO ACTION
+  FOREIGN KEY(cliCodigo) REFERENCES Cliente(cliCodigo),
+  FOREIGN KEY(anuCodigo) REFERENCES Anuncio(anuCodigo),
+  FOREIGN KEY(stpCodigo) REFERENCES StatusPedido(stpCodigo),
+  FOREIGN KEY(tivCodigo) REFERENCES TipoAvaliacoes(tivCodigo)
 );
-
 CREATE TABLE ItensPedido (
-  pedCodigo INTEGER UNSIGNED NOT NULL,
-  proCodigo INTEGER UNSIGNED NOT NULL,
+  pedCodigo INTEGER NOT NULL,
+  proCodigo INTEGER NOT NULL,
   itpValorVenda DECIMAL NOT NULL,
   itpValorCompra DECIMAL NOT NULL,
-  itpTaxa DECIMAL NOT NULL
-  FOREIGN KEY(proCodigo)
-    REFERENCES Produto(proCodigo)
-      ON DELETE NO ACTION
-      ON UPDATE NO ACTION,
-  FOREIGN KEY(pedCodigo)
-    REFERENCES Pedido(pedCodigo)
-      ON DELETE NO ACTION
-      ON UPDATE NO ACTION
+  itpTaxa DECIMAL NOT NULL,
+  FOREIGN KEY(proCodigo) REFERENCES Produto(proCodigo),
+  FOREIGN KEY(pedCodigo) REFERENCES Pedido(pedCodigo)
 );
-
-
--- DOCUMENTAÇÃO/Banco de Dados/SCRIPTS/DROP.sql
+++ DOCUMENTAÇÃO/Banco de Dados/SCRIPTS/DROP.sql
@@ -1,31 +1,15 @@
 DROP TABLE ItensPedido;
-
 DROP TABLE Pedido;
-
 DROP TABLE ItensEntrada;
-
 DROP TABLE Anuncio;
-
 DROP TABLE ProdutoEstoque;
-
 DROP TABLE TipoMovimentacao;
-
 DROP TABLE Entrada;
-
 DROP TABLE OutrasMovimentacoes;
-
 DROP TABLE Cliente;
-
 DROP TABLE TipoAnuncio;
-
 DROP TABLE TipoAvaliacoes;
-
 DROP TABLE Usuario;
-
 DROP TABLE Produto;
-
 DROP TABLE StatusAnuncio;
-
 DROP TABLE StatusPedido;
-
-
